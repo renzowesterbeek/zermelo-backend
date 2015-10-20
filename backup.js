11d5631cdea867bf4cd88fda46da14ed421725a7
Renzo Westerbeek <renzowesterbeek@gmail.com>
2015-10-20 12:50:39 +0200

    Create db backup script

---
+++ backup.js
@@ -0,0 +1,39 @@
+// backup.js
+// Backups files every x hours
+// Created on 20-10-2015
+// Status 0
+var fs = require('fs');
+var path = require('path');
+var ncp = require('ncp');
+
+function backupFile(backupdest, filename){
+  fs.readFile(filename, 'utf8', function (err, data) {
+    if (err) throw err;
+    fs.writeFile(backupdest + filename, data, function (err) {
+      if (err) throw err;
+      console.log(filename + ' saved!');
+    });
+  });
+}
+
+function zeroFix(num){
+  if(num < 10){
+    return "0" + num;
+  } else {
+    return num;
+  }
+}
+
+var date = new Date();
+var datestring = date.getFullYear() + "_" + zeroFix(date.getMonth()) + "_" + zeroFix(date.getDate()) + "-" + zeroFix(date.getHours()) + ":" + zeroFix(date.getHours());
+var backupdest = 'backup/' + datestring + '/';
+
+fs.mkdir('backup/', function(){
+  ncp('data/', backupdest, function(err){
+    if(err){
+      console.log(err);
+    } else {
+      console.log('Made backup @ ' + datestring);
+    }
+  });
+});
