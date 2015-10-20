036303ac5fb4b63b3bd0d7870d5a01295160773e
Renzo Westerbeek <renzowesterbeek@gmail.com>
2015-10-20 12:55:07 +0200

    Add interval to backup script

--- backup.js
+++ backup.js
@@ -1,11 +1,12 @@
 // backup.js
-// Backups files every x hours
+// Backups files every X hours
 // Created on 20-10-2015
 // Status 0
 var fs = require('fs');
 var path = require('path');
 var ncp = require('ncp');
 
+// backups one file a specified dest folder. function is for async purposes
 function backupFile(backupdest, filename){
   fs.readFile(filename, 'utf8', function (err, data) {
     if (err) throw err;
@@ -24,16 +25,18 @@ function zeroFix(num){
   }
 }
 
-var date = new Date();
-var datestring = date.getFullYear() + "_" + zeroFix(date.getMonth()) + "_" + zeroFix(date.getDate()) + "-" + zeroFix(date.getHours()) + ":" + zeroFix(date.getHours());
-var backupdest = 'backup/' + datestring + '/';
+setInterval(function(){
+  var date = new Date();
+  var datestring = date.getFullYear() + "_" + zeroFix(date.getMonth()) + "_" + zeroFix(date.getDate()) + "-" + zeroFix(date.getHours()) + ":" + zeroFix(date.getHours());
+  var backupdest = 'backup/' + datestring + '/';
 
-fs.mkdir('backup/', function(){
-  ncp('data/', backupdest, function(err){
-    if(err){
-      console.log(err);
-    } else {
-      console.log('Made backup @ ' + datestring);
-    }
+  fs.mkdir('backup/', function(){
+    ncp('data/', backupdest, function(err){
+      if(err){
+        console.log(err);
+      } else {
+        console.log('Made backup @ ' + datestring);
+      }
+    });
   });
-});
+}, 24 * 60 * 60 * 1000); // every 24 hours
